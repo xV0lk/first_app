a4a5f9baa231bfc13b1d61a99ebc22317731f2e6
jORGE <jorge.otto.415@gmail.com>
2021-09-13 05:13:05 -0500

    2. Services Section

---
+++ components/ServicesSection.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import clock from '../src/img/clock.svg';
+import diaphragm from '../src/img/diaphragm.svg';
+import money from '../src/img/money.svg';
+import teamwork from '../src/img/teamwork.svg';
+import home2 from '../src/img/home2.png';
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          Servicios de <span>alta</span> calidad.
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <Image src={clock} alt="clock icon" />
+              <h3>Efficientes y ágiles</h3>
+            </div>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <Image src={diaphragm} alt="diaphragm icon" />
+              <h3>Fotografía profesional</h3>
+            </div>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <Image src={money} alt="money icon" />
+              <h3>Soluciones de acuerdo a tu presupuesto</h3>
+            </div>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <Image src={teamwork} alt="teamwork icon" />
+              <h3>
+                Equipo de personas dedicado comprometido con tu crecimiento
+              </h3>
+            </div>
+          </div>
+          <p>Por qué elegirnos</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 function Home() {
   return (
-    <div>
-      <div>Hello World</div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 }
 
