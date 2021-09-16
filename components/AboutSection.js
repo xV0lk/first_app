3f3c83c7a79ad1b9ec0901fcd4f66823dfc0f73c
jORGE <jorge.otto.415@gmail.com>
2021-09-16 07:21:41 -0500

    4.1 Next Image tag style

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import home1 from '../src/img/home1.png';
+
 import styled from 'styled-components';
 
 const AboutSection = () => {
@@ -8,17 +8,17 @@ const AboutSection = () => {
     <SAbout>
       <SDescription>
         <div className="title">
-          <div className="hide">
+          <SHide>
             <h2>Hacemos</h2>
-          </div>
-          <div className="hide">
+          </SHide>
+          <SHide>
             <h2>
               tus <span>sueños</span>
             </h2>
-          </div>
-          <div className="hide">
+          </SHide>
+          <SHide>
             <h2>realidad.</h2>
-          </div>
+          </SHide>
         </div>
         <p>
           Cuéntanos sobre tu marca, somos profesionales capacitados para llevar
@@ -26,9 +26,16 @@ const AboutSection = () => {
         </p>
         <button>Contacto</button>
       </SDescription>
-      <SImage>
-        <Image src={home1} alt="Fotógrafo" />
-      </SImage>
+      <SIwrapper>
+        <div>
+          <Image
+            src="/home1.png"
+            alt="Fotógrafo"
+            objectFit="cover"
+            layout="fill"
+          />
+        </div>
+      </SIwrapper>
     </SAbout>
   );
 };
@@ -51,8 +58,18 @@ const SDescription = styled.div`
   }
 `;
 
-const SImage = styled.div`
+const SIwrapper = styled.div`
   flex: 1;
+  overflow: hidden;
+  div {
+    position: relative;
+    width: 100%;
+    height: 80vh;
+  }
+`;
+
+const SHide = styled.div`
+  overflow: hidden;
 `;
 
 export default AboutSection;
