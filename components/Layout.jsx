f2efaf197700164ab02cac920b787a144d9e6c6c
jORGE <jorge.otto.415@gmail.com>
2021-10-08 11:09:24 -0500

    9 First Deployment optimization3

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,15 @@
-import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import Navbar from './Navbar';
 
-const Layout = ({ children }) => {
-  const router = useRouter();
-  const pageTitle = router.pathname;
-  const titleArr = pageTitle.split('/');
-  const title = titleArr[1].charAt().toUpperCase() + titleArr[1].slice(1);
-  return (
-    <>
-      <NextSeo
-        title={title || undefined}
-        titleTemplate="Capture | %s"
-        description="Capture project es una página web donde mostramos nuestra compañía, servicios que ofrecemos y nuestros casos de éxito"
-      />
-      <Navbar />
-      <main>{children}</main>
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <NextSeo
+      title="Content"
+      description="Content project es una página web donde mostramos nuestra compañía, servicios que ofrecemos y nuestros casos de éxito"
+    />
+    <Navbar />
+    <main>{children}</main>
+  </>
+);
 
 export default Layout;
