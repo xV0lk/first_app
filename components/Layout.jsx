a27d7bc0ea44c602211c3baf68d5d3057d2f1cd2
jORGE <jorge.otto.415@gmail.com>
2021-10-08 23:15:43 -0500

    9 First Deployment optimization4

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,5 @@
 import { NextSeo } from 'next-seo';
 import React from 'react';
-import Navbar from './Navbar';
 
 const Layout = ({ children }) => (
   <>
@@ -8,7 +7,6 @@ const Layout = ({ children }) => (
       title="Content"
       description="Content project es una página web donde mostramos nuestra compañía, servicios que ofrecemos y nuestros casos de éxito"
     />
-    <Navbar />
     <main>{children}</main>
   </>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,23 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import GlobalStyle from '../styles/GlobalStyle';
 import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <Layout>
+    <>
       <DefaultSeo {...SEO} />
       <GlobalStyle />
-      <AnimatePresence
-        exitBeforeEnter
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </Layout>
+      <Layout>
+        <AnimatePresence
+          exitBeforeEnter
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Navbar />
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
