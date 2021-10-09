65980fed933b190967299f7c6668803cf347c97d
jORGE <jorge.otto.415@gmail.com>
2021-10-08 23:18:43 -0500

    9 First Deployment optimization4

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { AnimatePresence } from 'framer-motion';
-import { DefaultSeo } from 'next-seo';
+import { DefaultSeo, NextSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import GlobalStyle from '../styles/GlobalStyle';
-import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <NextSeo
+        title="Content"
+        description="Content project es una página web donde mostramos nuestra compañía, servicios que ofrecemos y nuestros casos de éxito"
+      />
       <GlobalStyle />
-      <Layout>
-        <Navbar />
-        <AnimatePresence
-          exitBeforeEnter
-          onExitComplete={() => window.scrollTo(0, 0)}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
+      <Navbar />
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
     </>
   );
 }
